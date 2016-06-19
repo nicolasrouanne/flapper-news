4329e111eb49c3815569699d044e6d231eb78e66
Nicolas Rouanne <nico.rouanne@gmail.com>
2016-06-19 15:46:25 +0200

    Opening REST Routes: GET and POST for Posts (/posts)

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
 // connect to the mongodb instance
 var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/news');
+
 require('./models/Posts');
 require('./models/Comments');
 
-mongoose.connect('mongodb://localhost/news');
+// launch routes
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
+var mongoose = require('mongoose');
 var express = require('express');
 var router = express.Router();
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET Posts */
+router.get('/posts', function(req, res, next) {
+	Post.find(function(err, posts) {
+		if(err) { return next(err); }
+
+		res.json(posts);
+	});
+});
+
+/* POST a Post */
+router.post('/posts', function(req, res, next) {
+	var post = new Post(req.body);
+
+	post.save(function(err, post) {
+		if(err) { return next(err); }
+
+		res.json(post);
+	});
 });
 
 module.exports = router;
