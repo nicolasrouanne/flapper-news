c032d4d6e90ee9285b1b8696758cc02b8daa06b6
Nicolas Rouanne <nico.rouanne@gmail.com>
2016-06-25 09:53:02 +0200

    Added persistent data for creating posts

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -39,6 +39,12 @@ function($stateProvider, $urlRouterProvider) {
 		})
 	}
 
+	o.create = function(post) {
+		return $http.post('/posts', post).success(function(data){
+			o.posts.push(data);
+		})
+	}
+
 	return o;
 }])
 
@@ -46,19 +52,13 @@ function($stateProvider, $urlRouterProvider) {
 	'$scope',
 	'posts',
 	function($scope, posts) {
-		$scope.test = 'Hello world!';
 		$scope.posts = posts.posts;
 
 		$scope.addPost = function() {
 			if (!$scope.title | $scope.title === '') {return; }
-			$scope.posts.push({
+			posts.create({
 				title: $scope.title,
-				link: $scope.link,
-				upvotes: 0,
-				comments: [
-					{author: 'Joe', body: 'Cool post!', upvotes: 0},
-					{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-				]
+				link: $scope.link
 			});
 			$scope.title = '';
 			$scope.link = '';
