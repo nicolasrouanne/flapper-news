2f777b7ad12f4ae109253eb45d7ea6b873819d77
Nicolas Rouanne <nico.rouanne@gmail.com>
2016-07-02 14:45:26 +0200

    Added method to generate JWT

--- models/Users.js
+++ models/Users.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
 var UserSchema = new mongoose.Schema ({
 	username: {type: String, lowercase: true, unique: true},
@@ -11,6 +12,28 @@ UserSchema.methods.setPassword = function(password) {
 	this.salt = crypto.randomBytes(16).toString('hex');
 	
 	this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
-}
+};
+
+UserSchema.methods.validPassword = function(password) {
+	var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+
+	return this.hash === hash;
+};
+
+UserSchema.methods.generateJWT = function() {
+
+	// set expiration to 60 days
+	var today = Date();
+	var exp = new Date(today);
+	exp.setDate(today.getDate() + 60);
+
+	// careful: secret variable of jwt is hardcoded
+	// TODO: replace it with an environment variable
+	return jwt.sign({
+		_id: this._id,
+		username: this.username,
+		exp: parseInt(exp.getDate() / 1000)
+	}, 'SECRET');
+};
 
 mongoose.model('User', UserSchema);
