122f2b985997ec55bfccc22d32d2e2a893939619
Nicolas Rouanne <nico.rouanne@gmail.com>
2016-07-03 14:50:54 +0200

    Added register and login methods to Auth controller

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -177,3 +177,27 @@ function($stateProvider, $urlRouterProvider) {
 		};
 
 }])
+
+.controller('AuthCtrl', [
+	'$scope',
+	'$state',
+	'auth',
+	function($scope, $state, auth) {
+		$scope.user = {};
+
+		$scope.register = function() {
+			auth.register($scope.user).error(function(error){
+				$scope.error = error;
+			}).then(function(){
+				$state.go('home');
+			});
+		};
+
+		$scope.logIn = function() {
+			auth.login($scope.user).error(function(error){
+				$scope.error = error;
+			}).then(function(){
+				$state.go('home');
+			});
+		};
+}])
