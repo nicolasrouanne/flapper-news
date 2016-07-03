1db64619c07216360a434e153fc1c106938d9079
Nicolas Rouanne <nico.rouanne@gmail.com>
2016-07-03 13:34:05 +0200

    Added POST route /register to create a new user

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var mongoose = require('mongoose');
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
 
 
 /* GET home page. */
@@ -102,5 +104,26 @@ router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
 		res.json(comment);
 	});
 }); 
+
+/* POST to create a new User */
+router.post('/register', function(req, res, next) {
+	if(!req.body.username || !req.body.password) { 
+		return res.status(400).json({message: 'Please fill in all the fields.'});
+	}
+
+	var user = new User();
+
+	user.username = req.body.username;
+	
+	user.setPassword(req.body.password);
+
+	user.save(function(err) {
+		if(err) { return next(err); }
+
+		return res.json({token: user.generateJWT()});
+	});
+
+
+});
 
 module.exports = router;
